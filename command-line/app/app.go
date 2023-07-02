16a24750e36f4d99f01a05078305ff2fbb452918
Fabio Marsiaj <fabiobackend@gmail.com>
2023-07-02 18:44:08 -0300

    SearchIp func

--- command-line/app/app.go
+++ command-line/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate will return the application cli to be executed
 func Generate() *cli.App {
@@ -8,5 +14,32 @@ func Generate() *cli.App {
 	app.Name = "CLI Application"
 	app.Usage = "Search for IPs and Server names on internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Search for IPs on internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context){
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
